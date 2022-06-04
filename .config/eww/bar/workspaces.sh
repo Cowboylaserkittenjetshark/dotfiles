1dba8b8999523702b64bc867f55d745ec8fa28ac
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-06-04 00:53:10 -0400

    Changes in eww in attempt to use Hyprland

--- .config/eww/bar/workspaces.sh
+++ .config/eww/bar/workspaces.sh
@@ -1,25 +1,26 @@
-#!/usr/bin/env bash
-gen_workspace_literal() {
-  current_workspace=""
-  wmctrl -d | awk '{ print $1 " " $2 " " $9 }' | grep -v NSP | while read -r id active name; do
-    name="${name#*_}"
-    if wmctrl -l | grep --regexp '.*\s\+'"$id"'\s\+.*' >/dev/null; then
+#!/bin/sh
+current_workspace=""
+gen_workspace_literal () {
+  if [ $(echo $1 | awk -F '>' '{print $1}') == "workspace" ]; then
+    current_workspace="$(echo $1 | awk -F '>' '{print $3}')"
+  fi
+  for id in {1..5}; do
+    if hyprctl workspaces | grep "workspace ID $id" >/dev/null; then
       button_class="occupied"
       button_name=""
     else
       button_class="empty"
       button_name=""
     fi
-    if [ "$active" == '*' ]; then
+    if [[ $current_workspace == $id ]]; then
       active_class="active"
       button_name=""
     else
       active_class="inactive"
     fi
-    echo -n "(button :class \"$button_class $active_class\"  :onclick \"wmctrl -s $id\" \"$button_name\")"
+    echo -n "(button :class \"$button_class $active_class\" \"$button_name\")"
   done
 }
-xprop -spy -root | while read -r; do
-  echo '(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal)"')'
-  # gen_workspace_literal
+socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read -r line; do
+eww update wm_workspaces="$(echo '(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal $line)"')')"
 done
