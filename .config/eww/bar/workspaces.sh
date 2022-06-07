633ddb7da36bc106fda2b3228b4b14de33e5411f
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-06-07 15:14:29 -0400

    Moved eww launcher to a script

--- .config/eww/bar/workspaces.sh
+++ .config/eww/bar/workspaces.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-current_workspace=""
 gen_workspace_literal () {
   if [ $(echo $1 | awk -F '>' '{print $1}') == "workspace" ]; then
     current_workspace="$(echo $1 | awk -F '>' '{print $3}')"
@@ -21,6 +20,8 @@ gen_workspace_literal () {
     echo -n "(button :class \"$button_class $active_class\" \"$button_name\")"
   done
 }
-socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read -r line; do
-eww update wm_workspaces="$(echo '(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal $line)"')')"
+  current_workspace=""
+socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock |  while read -r line; do
+#eww update wm_workspaces="$(echo '(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal $line)"')')"
+echo '(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal $line)"')'
 done
