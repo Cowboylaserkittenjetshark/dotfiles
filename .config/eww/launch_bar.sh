9a16e673fb9b64227857ef753f38a4a61c76653f
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-07-28 16:27:58 -0400

    Fix bar launcher again

--- .config/eww/launch_bar.sh
+++ .config/eww/launch_bar.sh
@@ -5,6 +5,6 @@ eww kill
   if [ -d /sys/class/power_supply/BAT*/ ]; then
     eww update is_laptop=true
   fi
-pkill -f workspaces.sh
+pkill -KILL -f workspaces.sh
 while pgrep -u $UID -x workspaces.sh >/dev/null; do sleep 1; done
 "$XDG_CONFIG_HOME"/eww/bar/workspaces.sh
