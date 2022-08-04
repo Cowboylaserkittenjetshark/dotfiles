186a5cc2eed2b8542f2f3a16131264aaa23bb437
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-08-03 22:34:03 -0400

    Add sleep to monitor setup script exec to work around freeze

--- .config/hypr/hyprland.sh
+++ .config/hypr/hyprland.sh
@@ -2,6 +2,6 @@
 for dir in /sys/class/drm/card?-*
 do 
   if grep enabled "$dir"/enabled >/dev/null; then
-    hyprctl keyword monitor "$(echo "$dir" | sed 's/\/sys\/class\/drm\/card0-//')","$(sed -n '1p' "$dir"/modes)"@60,0x0,1
+    echo keyword monitor "$(echo "$dir" | sed 's/\/sys\/class\/drm\/card0-//')","$(sed -n '1p' "$dir"/modes)"@60,0x0,1 | socat - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket.sock
   fi
 done
