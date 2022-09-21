dc8f757242abf5e94136848e43dc3e405fb4b506
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-09-21 17:15:19 -0400

    Remove all vol_scroll stuff, desired behavior is implemented in scale widget

--- .config/eww/bar/vol_scroll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$2" == "up" ]; then
-  pactl set-sink-volume "$1" +1%
-else
-  pactl set-sink-volume "$1" -1%
-fi
-eww update curr_vol="$(pactl get-sink-volume "$1" | awk '{print $5}')"
