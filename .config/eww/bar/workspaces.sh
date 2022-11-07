0db5c211fb0f86e8f512c04dc878f9f0dce1ec35
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-11-06 22:48:28 -0500

    Start rewriting hyprland workspaces script

--- .config/eww/bar/workspaces.sh
+++ .config/eww/bar/workspaces.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
-gen_workspace_literal () {
-  # CURRENT_WORKSPACE="$(hyprctl activewindow | grep workspace | awk '{print $2}')"
-  for id in {1..5}; do
-    if hyprctl workspaces | grep -A1 "workspace ID $id" | grep -v -e "0" -e "^$" -e "workspace ID" >/dev/null; then
-      button_class="occupied"
-      button_name=""
-    else
-      button_class="empty"
-      button_name=""
-    fi
-    if [ "$CURRENT_WORKSPACE" = "$id" ]; then
-      active_class="active"
-      button_name=""
-    else
-      active_class="inactive"
-    fi
-    echo -n "(button :class \"$button_class $active_class\" :onclick \"hyprctl dispatch workspace $id\" \"$button_name\")"
+help() {
+  echo "Usage: $0 [OPTIONS] num_workspaces"
+  echo "Options:"
+  echo "    -h    Print help text"
+}
+
+generate_json() {
+  for ((index=1; index <= WORKSPACES; index++)); do
+    echo "{}"
   done
 }
-while read -r line; do
-  if grep ^workspace <<< "$line" >/dev/null; then
-    CURRENT_WORKSPACE="$(awk -F '>' '{print $3}' <<< "$line")"
-  fi
-  eww update $1='(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal "$line")"')'
-  # echo '(box :orientation "h" :class "workspaces" :space-evenly true :halign "center" :valign "center" :hexpand true '"$(gen_workspace_literal "$line")"')'
-done < <(socat - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock)
+
+while getopts "hn:" option; do
+  case $option in
+    h)
+      help
+      exit 0
+      ;;
+    ?)
+      help
+      exit 2
+      ;;
+  esac
+done
+shift "$((OPTIND -1))"
+if [ -z "$1" ]; then
+  help
+  exit 2
+fi
+WORKSPACES=$1
+echo "$WORKSPACES"
+# while read -r line; do
+#   qewrqewrewqr
+# done < <(socat - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock)
+
