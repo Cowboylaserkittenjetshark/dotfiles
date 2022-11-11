ee6733cf84c42b1b0a2f6d9a8da9bd5753083d33
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-11-11 15:44:07 -0500

    Return initial json before any events

--- .config/eww/scripts/workspaces.sh
+++ .config/eww/scripts/workspaces.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+getJson() {
+  jq --null-input --compact-output\
+    --argjson MONITORS "$(hyprctl -j monitors)"\
+    --argjson WORKSPACES "$(hyprctl -j workspaces | jq 'sort_by(.id)')"\
+    '{"monitors": $MONITORS, "workspaces": $WORKSPACES}'
+  }
+getJson
 while read -r line; do
   if [[ ${line:0:9} == "workspace" ]]; then
-    jq --null-input --compact-output\
-      --argjson MONITORS "$(hyprctl -j monitors)"\
-      --argjson WORKSPACES "$(hyprctl -j workspaces | jq 'sort_by(.id)')"\
-      '{"monitors": $MONITORS, "workspaces": $WORKSPACES}'
+    getJson
   fi
 done < <(socat -U - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock)
