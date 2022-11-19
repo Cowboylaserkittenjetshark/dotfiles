71239fd465d45e6bd8a9311b97080dff1c85c7a9
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2022-11-19 11:56:19 -0500

    Remove monitor script

--- .config/hypr/hyprland.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for dir in /sys/class/drm/card?-*
-do 
-  if grep enabled "$dir"/enabled >/dev/null; then
-    echo keyword monitor "$(echo "$dir" | sed 's/\/sys\/class\/drm\/card0-//')","$(sed -n '1p' "$dir"/modes)"@60,0x0,1 | socat - UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket.sock
-  fi
-done
