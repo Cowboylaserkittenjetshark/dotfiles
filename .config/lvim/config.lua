00b7db5a87f1ca18c8735c9db673f2a69dda6188
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-07-13 14:25:35 -0400

    Lvim dap config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -193,3 +193,28 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+local dap = require('dap')
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/") .. "bin/codelldb",
+    args = { "--port", "${port}" },
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
+dap.configurations.cpp = {
+  {
+    name = 'Launch cpp debug',
+    type = 'codelldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+}
